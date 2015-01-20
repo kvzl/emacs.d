3f0793752cc6f0952ccddae3497b2da6412b5a3d
ucfan <ucfan1784@gmail.com>
2015-01-21 02:43:08 +0800

    modify some setting of web mode

--- init.d/init-web-mode.el
+++ init.d/init-web-mode.el
@@ -8,6 +8,9 @@
 			    (setq web-mode-code-indent-offset 4)
 			    (setq web-mode-indent-style 4)
 			    (setq web-mode-enable-current-element-highlight t)
+			    (setq web-mode-enable-auto-pairing t)
+			    (setq web-mode-enable-auto-closing t)
+			    (setq web-mode-enable-auto-indentation t)
 			    (setq web-mode-ac-sources-alist
 				  '(("css" . (ac-source-css-property))
 				    ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
